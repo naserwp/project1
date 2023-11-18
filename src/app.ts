1b774f155c4f5758a4df23a9dbf93caf93faa153
naserwp <naserwp@gmail.com>
2023-11-18 08:27:31 +0600

    file on git fixed

---
+++ src/app.ts
@@ -0,0 +1,16 @@
+import cors from 'cors';
+import express, { Application, Request, Response } from 'express';
+
+const app: Application = express();
+// const port = 3000
+
+//parsers
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req: Request, res: Response) => {
+  const a = 30;
+  res.send(a);
+});
+
+export default app;
